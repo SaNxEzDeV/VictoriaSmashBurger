82440bde1fea8b5a589aee76a965f5a949b6f994
Rafael Sanchez <rafael.sanchez@homeserve.com>
2025-02-13 14:35:05 +0100

    added menus

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function openPDF(language) {
     const pdfLinks = {
-        en: "https://example.com/english-menu.pdf", // Replace with your actual link
-        es: "https://example.com/spanish-menu.pdf", // Replace with your actual link
-        de: "https://example.com/german-menu.pdf"  // Replace with your actual link
+        en: "https://drive.google.com/file/d/1pDdXWaE0kARqrRWejz6Mt9-Cw6CKm6D2/view", // Replace with your actual link
+        es: "https://drive.google.com/file/d/10ETIDWi3RwYVWKjXzBAuv8shUZoZVax8/view", // Replace with your actual link
+        de: "https://drive.google.com/file/d/1JDaM4jFjzL-hEmVFaCLFOzRmanqhyczt/view"  // Replace with your actual link
     };
     
     const pdfUrl = pdfLinks[language];
